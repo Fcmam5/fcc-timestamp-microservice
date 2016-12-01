1444fc24450878f307cfd1d62aec2bfda12f0081
fcmam5 <dr.fcmam5@gmail.com>
2016-12-01 02:15:14 +0100

    first solution

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,23 @@
 module.exports = function(app){
-app.get('/:timestamp', function (req, res) {
-  res.send('Hello ! '+req.params.timestamp)
-})
+    app.get('/:timestamp', function (req, res) {
+        var attr = req.params.timestamp;
+        var obj = {
+            unix : null,
+            natural: null
+        }
+        if(/^\d+$/.test(attr)){
+            var myDate = new Date(attr*1000);
+            if (myDate.getTime()>0) {
+                obj.unix = attr;
+                obj.natural = myDate.toString();
+            }
+        }else {
+            if (Date.parse(attr)) {
+                obj.unix = Date.parse(attr);
+                obj.natural = attr;
+            }
+        }
+
+      res.json(obj)
+    })
 };
